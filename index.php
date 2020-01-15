548af654a12ef7593fbf778920aeaae41dce21b0
Rob Wood <rwood3@mail.greenriver.edu>
2020-01-15 14:34:54 -0500

    added views and set up to view home.html

--- index.php
+++ index.php
@@ -14,4 +14,16 @@ error_reporting(E_ALL);
 // require the autoload file
 require('vendor/autoload.php');
 
+// Create an instance of the base class
+$f3 = Base::instance();
+
+// define a default route
+$f3 -> route('GET /', function(){
+    $view = new Template();
+    echo $view->render('views/home.html');
+
+});
+// Run Fat Free
+$f3 -> run();
+
 
